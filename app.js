28c5145cc8a245eeabcecf5ac951d84c374c8b9e
jcoppede <jjoseneyra6@gmail.com>
2022-02-13 16:50:51 -0300

    empezando detalleProducto

--- app.js
+++ app.js
@@ -16,3 +16,7 @@ app.get("/", (req, res) => {
 app.get("/productCart", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./src/views/productCart.html"));
 });
+
+app.get("/productDetail", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./src/views/productDetail.html"));
+});
