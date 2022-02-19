fd70744f5a5cf0041d78b265692e9d340ae650c4
clonox2 clonox2 <emiliogalimberty@gmail.com>
2022-02-19 02:35:41 -0300

    add partials/headerMobile, add array en webcontroller

--- src/controllers/webControllers.js
+++ src/controllers/webControllers.js
@@ -1,14 +1,16 @@
 const path = require('path');
 
+let titulos = ['CARRITO', "HOME", "PRODUCTS","SING UP"]
+
 module.exports = {
     index: (req,res) =>{
-        res.render(path.resolve(__dirname, '../views/web/index'));
+        res.render(path.resolve(__dirname, '../views/web/index'),{titulos, "numero":1});
     },
     compra: (req,res) =>{
-        res.render(path.resolve(__dirname, '../views/products/productCart'));
+        res.render(path.resolve(__dirname, '../views/products/productCart'),{titulos, "numero":0});
     },
     products:(req,res) =>{
-        res.render(path.resolve(__dirname, '../views/products/productDetail'));
+        res.render(path.resolve(__dirname, '../views/products/productDetail'),{titulos, "numero":2});
     },
     /*login:(req,res) =>{
         res.render(path.resolve(__dirname, '../views/users/login'));
