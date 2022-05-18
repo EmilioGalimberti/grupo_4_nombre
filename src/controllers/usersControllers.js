3888b96abf6700b167329977272b3194b7650b1e
Alexandra <alexandrasalgan@gmail.com>
2022-05-17 21:40:29 -0300

    implementar_registro_usuarios

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const bcrypt = require('bcryptjs');
 
 const session = require('express-session');
 
@@ -15,7 +16,38 @@ const usersControllers = {
 				session.email = req.body.email;
 				let email = session.email;
 				res.render(path.resolve(__dirname, '../views/web/index'),{titulos, "numero":1, email});
+		},
+		register:(req, res) =>{
+			res.render('users/register',{titulos, "numero":4});
+		},
+		userCreate: (req, res) => {
+			res.send(req.body)
+			if(req.file.filename){
+				const hash = bcrypt.hashSync(req.body.password, 8);
+				let newUser = {
+					id: users[users.length - 1].id + 1,
+					firstName: req.body.firstName,
+                 	lastName:req.body.lastName,
+                 	email:req.body.email,
+                 	password: hash,
+					image: "/images/usersData/"+req.file.filename
+				};
+				users.push(newUser);
+				fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
+				res.redirect('/users');
+			}
+			else{
+				let newUser = {
+					id: users[users.length - 1].id + 1,
+					...req.body,
+					image: "/images/usersData/default-image.png"
+				};
+				users.push(newUser);
+				fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
+				res.redirect('/users');   
+			}  
 		}
+		
 }
 
 module.exports = usersControllers;
--- src/routes/webRoutes.js
+++ src/routes/webRoutes.js
@@ -11,6 +11,21 @@ router.get('/carrito', webControllers.carrito);
 router.get('/detail', webControllers.detail);
 router.get('/login', webControllers.login);
 
+
+const storage = multer.diskStorage({
+    destination: function(req, res, cb){
+        cb(null, path.join(__dirname, "../../public/images/usersData"))
+    },
+    filename: function (req, file, cb){
+        cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`)
+    }
+});
+const uploadFile = multer({storage});
+
+router.get('/register',usersControllers.register);
+router.post('/register', uploadFile.single("image"),usersControllers.userCreate);
+
+
 router.post('/', userMidd, usersControllers.acceso)
 
 module.exports = router;
